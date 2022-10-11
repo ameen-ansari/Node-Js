c4065d8a981963d97204719839d6645b4d30ac14
M.Ameen <ameen55668@gmail.com>
2022-10-11 23:07:51 +0500

    add express

--- app.js
+++ app.js
@@ -11,12 +11,25 @@
 // a = os.type()
 // a = os
 // console.log(a);
+//Creating A server
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.statusCode = 200
+//     res.setHeader("Content-Type", "text/html")
+//     res.end('<h1>Hello World</h1><p>hello Try First backend Web On Browser</p>')
+// }).listen(80, '127.1.2.3', () => {
+//     console.log("Server Started...");
+// })
 
-const http = require('http')
-http.createServer((req , res )=>{
-    res.statusCode = 200
-    res.setHeader("Content-Type","text/html")
-    res.end('<h1>Hello World</h1><p>hello Try First backend Web On Browser</p>')
-}).listen(8000,()=> {
-    console.log("Server Started...");
+const express = require('express')
+let app = express()
+app.get("/",(req , res)=>{
+res.send('Hello World')
 })
+app.get("/about",(req , res)=>{
+res.send('Welcome in about')
+})
+app.get("/servises",(req , res)=>{
+res.send('Welcome in services')
+})
+app.listen(80)

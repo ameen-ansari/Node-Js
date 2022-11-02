33f88254ea0701168e661d25230168c0d4778405
M.Ameen <ameen55668@gmail.com>
2022-11-02 20:37:47 +0500

    project  updated

--- todo/src/Apis/Api.js
+++ todo/src/Apis/Api.js
@@ -1,11 +1,16 @@
 import axios from 'axios'
 let senddata = async (x) => {
-    await axios.post("http://localhost:4000/", x)
+    await axios.post("http://localhost:4000/users", x)
 }
 
-let receivedata = async ( ) => {
+let receivedata = async () => {
     let data = await axios.get('http://localhost:4000/users')
     return data.data
 }
 
-export { senddata, receivedata }
+let deletedata = async () => {
+    await axios.get('http://localhost:4000/users/reset')
+}
+
+
+export { senddata, receivedata, deletedata }
--- todo/src/Components/SignUp.js
+++ todo/src/Components/SignUp.js
@@ -1,8 +1,7 @@
 import style1 from './Signup.module.css'
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import { receivedata, senddata } from '../Apis/Api'
-
+import React, { useState } from 'react'
+import axios from 'axios'
+// import { senddata, receivedata, deletedata } from '../Apis/Api'
 export default function SignUp() {
     const [userAlldata, setuserAlldata] = useState([])
 
@@ -14,21 +13,7 @@ export default function SignUp() {
     }
 
     const [udata, setudata] = useState(formValues)
-
-    // let mydata = async () => {
-    //     let data = await receivedata()
-    //     setuserAlldata(data)
-    //     console.log(userAlldata);
-    // }
-
-    let receivedata = async () => {
-        let data = await axios.get('http://localhost:4000/users')
-        setuserAlldata(data.data)
-        console.log(data.data)
-    }
-
-
-    let userdata = (e) => {
+    let makeUserObj = (e) => {
         let input = {
             [e.target.name]: e.target.value
         }
@@ -36,22 +21,24 @@ export default function SignUp() {
             ...udata, ...input
         })
     }
-    let Sup = async (e) => {
+
+    let onSubmitH = async (e) => {
         e.preventDefault()
-        try {
-            setudata(formValues)
-            alert('User Added')
-            senddata(udata)
-            receivedata()
-            // mydata()
-        } catch (error) {
-            console.log("error", error);
-        }
+        await axios.post('http://localhost:4000/users' , udata )
     }
-    useEffect(() => {
-            // mydata()
-            receivedata()
-    }, [])
+    
+    let fetchdata = async (e) => {
+        e.preventDefault()
+        let data = await axios.get('http://localhost:4000/users')
+        setuserAlldata(data.data)
+        console.log('StateValue',userAlldata);
+        console.log('From Request',data.data)
+    }
+
+    let reset = async (e) => {
+        e.preventDefault()
+        let data = await axios.get('http://localhost:4000/users/reset')
+    }
     let stylishobj1 = { flexDirection: 'column', display: 'flex', boxSizing: 'border-box', alignIitems: 'center', justifyContent: 'center', height: '100vh' }
     return (
         <div className={style1.parent1}>
@@ -60,42 +47,40 @@ export default function SignUp() {
                 <div className={` ${style1.p2}`}>
                     <div className="m-2 ">
                         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input onChange={userdata} name="mail" udata={udata.mail} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Email Here' />
+                        <input onChange={makeUserObj} name="mail" udata={udata.mail} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Email Here' />
                     </div>
                     <div className="m-2 ">
                         <label htmlFor="exampleInputEmail2" className="form-label">UserName</label>
-                        <input value={udata.UserName} name="UserName" onChange={userdata} type="text" className="form-control" id="exampleInputEmail2" aria-describedby="emailHelp" placeholder='Enter Name Here' />
+                        <input value={udata.UserName} name="UserName" onChange={makeUserObj} type="text" className="form-control" id="exampleInputEmail2" aria-describedby="emailHelp" placeholder='Enter Name Here' />
                     </div>
                 </div>
                 <div className={` ${style1.p2}`}>
                     <div className="m-2 ">
                         <label htmlFor="exampleInputEmail3" className="form-label">Phone #</label>
-                        <input onChange={userdata} value={udata.phoneNumber} name="phoneNumber" type="number" className="form-control" id="exampleInputEmail3" aria-describedby="emailHelp" placeholder='Phone Number' />
+                        <input onChange={makeUserObj} value={udata.phoneNumber} name="phoneNumber" type="number" className="form-control" id="exampleInputEmail3" aria-describedby="emailHelp" placeholder='Phone Number' />
                     </div>
                     <div className="m-2 ">
                         <label htmlFor="exampleInputEmail31" className="form-label">Password</label>
-                        <input onChange={userdata} name="password" value={udata.password} type="password" className="form-control" id="exampleInputEmail31" aria-describedby="emailHelp" placeholder='Enter Password Here' />
+                        <input onChange={makeUserObj} name="password" value={udata.password} type="password" className="form-control" id="exampleInputEmail31" aria-describedby="emailHelp" placeholder='Enter Password Here' />
                     </div>
                 </div>
                 <div className={`mb-3 ${style1.p3}`}>
-                    <button type="submit" className="m-2 btn btn-primary" onClick={Sup}>Create Acount</button>
+                    <button type="submit" className="m-2 btn btn-primary" onClick={onSubmitH}>Save</button>
+                    <button  className="m-2 btn btn-primary" onClick={fetchdata}>fetchdata</button>
+                    <button className="m-2 btn btn-primary" onClick={reset}>Reset</button>
                 </div>
-                <div>
+                {/* <div>
                     {
                         userAlldata.map((obj, i) => {
                             return (
                                 <div key={i}>
                                     <p >User Name = {obj.UserName}</p>
-                                    <p>user PhoneNumber = {obj.phoneNumber}</p>
-                                    <p>user mail = {obj.phoneNumber}</p>
-                                    <p>user Password = {obj.password}</p>
                                     <hr />
                                 </div>
                             )
-
                         })
                     }
-                </div>
+                </div> */}
             </form>
         </div >
     )
